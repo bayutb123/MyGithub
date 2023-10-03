4e616c8a2230b69986b8bd7de06933706b457aaf
Bayu Tantra Bramandhita <73697375+bayutb123@users.noreply.github.com>
2023-10-03 23:28:49 +0700

    Add copy url FAB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,8 @@ dependencies {
     implementation( "com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("androidx.compose.material:material-icons-extended-android:1.5.2")
+
     implementation("com.airbnb.android:lottie-compose:6.1.0")
 
     implementation("io.coil-kt:coil-compose:2.4.0")
--- app/src/main/java/com/bayutb123/mygithub/presentation/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/bayutb123/mygithub/presentation/screen/detail/DetailScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,10 +22,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.bayutb123.mygithub.data.source.state.UserDetailState
+import com.bayutb123.mygithub.domain.model.UserDetail
 import com.bayutb123.mygithub.presentation.screen.home.components.LoadingAnimation
 
 @Composable
@@ -30,14 +37,31 @@ fun DetailScreen(
     userName: String
 ) {
     val detailViewModel = hiltViewModel<DetailViewModel>()
+    val clipboardManager = LocalClipboardManager.current
     detailViewModel.getUserDetail(userName)
-    Scaffold { paddingValues ->
+    var githubUrl = ""
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                clipboardManager.setText(AnnotatedString(githubUrl))
+            }) {
+                Icon(
+                    imageVector = Icons.Default.ContentCopy,
+                    contentDescription = "Share"
+                )
+            }
+        }
+    ) { paddingValues ->
         Column(
             modifier
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            UserInfo(user = detailViewModel.state.collectAsState().value)
+            UserInfo(
+                user = detailViewModel.state.collectAsState().value,
+                getUserGithubUrl = {
+                    githubUrl = it
+                })
         }
     }
 }
@@ -45,7 +69,8 @@ fun DetailScreen(
 @Composable
 fun UserInfo(
     modifier: Modifier = Modifier,
-    user: UserDetailState
+    user: UserDetailState,
+    getUserGithubUrl: (String) -> Unit
 ) {
     when (user) {
         is UserDetailState.Loading -> {
@@ -55,30 +80,50 @@ fun UserInfo(
         }
 
         is UserDetailState.Success -> {
-            Box(modifier = modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.primary)
-                .padding(16.dp)) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    AsyncImage(
-                        modifier = modifier.size(128.dp).clip(CircleShape),
-                        model = user.data.avatarUrl,
-                        contentDescription = user.data.name
-                    )
-                    Column(modifier = modifier) {
-                        Text(text = user.data.name, color = MaterialTheme.colorScheme.onPrimary, style = MaterialTheme.typography.headlineSmall)
-                        Text(text = user.data.login, color = MaterialTheme.colorScheme.onPrimary, style = MaterialTheme.typography.bodyMedium)
-                        user.data.location?.let { Text(text = it, color = MaterialTheme.colorScheme.onPrimary) }
-                    }
-                }
+            getUserGithubUrl(user.data.htmlUrl)
+            Column {
+                ProfileSection(user = user.data, modifier = modifier)
             }
         }
 
         is UserDetailState.Error -> {
             Text(text = user.errorMessage)
+        }
+    }
+}
+
+@Composable
+fun ProfileSection(user: UserDetail, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            AsyncImage(
+                modifier = modifier
+                    .size(128.dp)
+                    .clip(CircleShape),
+                model = user.avatarUrl,
+                contentDescription = user.name
+            )
+            Column(modifier = modifier) {
+                Text(
+                    text = user.name,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = user.login,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                user.location?.let { Text(text = it, color = MaterialTheme.colorScheme.onPrimary) }
+            }
         }
     }
 }
